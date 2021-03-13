e1c14a40fa9d1093b9a0dffbb2172c44b0aaedc2
Satoszi <stickyy6@gmail.com>
2021-03-13 03:55:10 +0100

    reduced some functions and components

--- src/Choosing.js
+++ src/Choosing.js
@@ -2,28 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class WordsListToLearn extends React.Component {
-
-  render() {
-    let wordsListToShow = null
-    if (this.props.wordsListToLearn !== null) 
-    wordsListToShow = this.props.wordsListToLearn.map(e => {return (
-    <div onClick={()=>this.props.moveElementBack(e.english)} className="translating" key = {e.english}>
-      <div className="englishTranslating" >{e.english}</div> 
-      <div className="polishTranslating">{e.polish}</div>
-      </div>)
-  })
-
-    return (
-      <div> 
-          {wordsListToShow}
-      </div>        
-    );
-  }
-} 
-
-
 class WordsList extends React.Component {
 
   render() {
@@ -54,38 +32,28 @@ export class ChoosingWords extends React.Component {
   }
   
 
-  async getApi(){
-    //const random = Math.floor(Math.random() * 100) + 100; 
-    const response = await fetch( "http://bitex122.vot.pl/getuserwordsnotlearned.php?userid=9&from= " + 100 + "&to= " + 135);
+  async getAp(url, stateVar){
+    const response = await fetch( url);
     const data = await response.json();
   
     const arr = []
     for (var key in data){
       arr.push({english: key, polish: data[key] })
     }
-      //this.state.wordsListToLearn.includes(element)
-      
-    this.setState({ wordsList: arr})
-  }
-
-  async getApiToLearn(){
-  
-    const response = await fetch( "http://bitex122.vot.pl/getuserwordsbystatus.php?userid=9&status=0");
-    const data = await response.json();
-  
-    const arr = []
-    for (var key in data){
-      arr.push({english: key, polish: data[key] })
+    switch(stateVar) {
+      case "wordsList":
+        this.setState({ wordsList: arr})
+        break;
+      case "wordsListToLearn":
+        this.setState({ wordsListToLearn: arr})
+        break;
     }
-
-    this.setState({ wordsListToLearn: arr})
+    
   }
 
   async componentDidMount(){
-
-    this.getApiToLearn()
-    this.getApi()
-    
+    this.getAp("http://bitex122.vot.pl/getuserwordsnotlearned.php?userid=9&from= " + 100 + "&to= " + 135, "wordsList")
+    this.getAp("http://bitex122.vot.pl/getuserwordsbystatus.php?userid=9&status=0", "wordsListToLearn")
   }
 
 moveElement = (english) =>{
@@ -126,7 +94,7 @@ fetch( "http://bitex122.vot.pl/deletefromstatus.php?userid=9&engword=" + english
         <div className="choosingBox"> 
         <div> Choosed words  </div>
         <br/>
-        <WordsListToLearn wordsListToLearn = {this.state.wordsListToLearn} moveElementBack ={this.moveElementBack} />
+        <WordsList wordsList = {this.state.wordsListToLearn} moveElement={this.moveElementBack}/>
 
         </div> 
 
